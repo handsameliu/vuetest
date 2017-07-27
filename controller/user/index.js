c2105d6a1a6ced9924eb468d86a5b8b6a94b5bd4
handsameliu <841489295@qq.com>
2017-07-27 12:03:48 +0800

    增加备注

--- controller/user/index.js
+++ controller/user/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 let service = require('../../service');
+let co =require('co');
 let user = service.user;
 
 exports.signIn = (req,res)=>{
 	user.signIn(req,res);
 
-	/**
+	/** 例：
 	 * co(user.signIn({req.body.username,req.body.password})).then((data) => {
         res.json(message(null, {result: data}))
 		}).catch((error) => {
